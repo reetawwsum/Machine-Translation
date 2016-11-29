8b6d552d077a94522ea5e3ac0c46ed6f5302ed24
Reet Awwsum <reetawwsum@yahoo.com>
2016-11-29 07:05:57 +0530

    Added train op to the graph

--- model.py
+++ model.py
@@ -14,6 +14,8 @@ class Model():
 		self.num_units = config.num_units
 		self.num_hidden_layers = config.num_hidden_layers
 		self.learning_rate = config.learning_rate
+		self.learning_rate_decay_factor = config.learning_rate_decay_factor
+		self.max_gradient_norm = config.max_gradient_norm
 		self.num_samples = config.num_samples
 		self.buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
 
@@ -39,7 +41,17 @@ class Model():
 		self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(self.encoder_inputs, self.decoder_inputs, self.targets, self.target_weights, self.buckets, lambda x, y: seq2seq_f(x, y), self.softmax_loss_function)
 
 	def train_op(self):
-		pass
+		params = tf.trainable_variables()
+		self.gradient_norms = []
+		self.updates = []
+		
+		opt = tf.train.GradientDescentOptimizer(self.learning_rate_var)
+
+		for b in xrange(len(self.buckets)):
+			gradients = tf.gradients(self.losses[b], params)
+			clipped_gradients, norm = tf.clip_by_global_norm(gradients, self.max_gradient_norm)
+			self.gradient_norms.append(norm)
+			self.updates.append(opt.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step))
 
 	def create_saver(self):
 		saver = tf.train.saver()
@@ -62,8 +74,16 @@ class Model():
 			# Creating output projection and softmax loss function in order to handle large vocabulary
 			self.output_projection, self.softmax_loss_function = handle_large_vocabulary(self.num_samples, self.num_units, self.target_vocab_size)
 
+			# Creating learning rate variable, learning rate decay op, and global step for train op
+			self.learning_rate_var = tf.Variable(float(self.learning_rate), trainable=False, dtype=tf.float32)
+			self.learning_rate_decay_op = self.learning_rate_var.assign(self.learning_rate_var * self.learning_rate_decay_factor)
+			self.global_step = tf.Variable(0, trainable=False)
+
 			# Builds the graph that computes inference and loss
 			self.inference_and_loss()
+
+			# Adding train op to the graph
+			self.train_op()
 
 	def train(self):
 		pass
